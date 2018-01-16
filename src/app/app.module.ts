aae8155cc98e585dadc1d9a243debef9b1e847bd
Douglas Carlos <dnevescarlos@gmail.com>
2018-01-15 23:45:37 -0200

    criando modulo conversor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { ConversorModule } from './conversor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ConversorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
