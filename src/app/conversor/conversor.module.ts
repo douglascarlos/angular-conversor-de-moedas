5fa35d8997a4a21ff8fd0ff4fd2c74b9a1fe8033
Douglas Carlos <dnevescarlos@gmail.com>
2018-01-16 00:04:42 -0200

    criando componente do conversor

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ConversorComponent } from './components';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+  	ConversorComponent
+  ],
+  exports: [
+  	ConversorComponent
+  ]
 })
 export class ConversorModule { }
