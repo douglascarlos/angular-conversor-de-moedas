7f290b0ad7177737c786522600570b343704885b
Douglas Carlos <dnevescarlos@gmail.com>
2018-01-16 01:08:00 -0200

    criando service de conversão

--- src/app/conversor/components/conversor.component.ts
+++ src/app/conversor/components/conversor.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { MoedaService } from '../services';
-
 @Component({
   selector: 'app-conversor',
   templateUrl: './conversor.component.html',
@@ -9,11 +7,9 @@ import { MoedaService } from '../services';
 })
 export class ConversorComponent implements OnInit {
 
-  constructor(private moedaService: MoedaService) { }
+  constructor() { }
 
   ngOnInit() {
-  	console.log(this.moedaService.listarTodas());
-  	console.log(this.moedaService.listarTodas().pop());
   }
 
 }
