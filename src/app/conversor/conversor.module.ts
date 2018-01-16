eb59d33b6e5369138dcf663b0489ef55e3dc6f5b
Douglas Carlos <dnevescarlos@gmail.com>
2018-01-16 00:10:05 -0200

    criando serviço para moedas

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ConversorComponent } from './components';
+import { MoedaService } from './services';
 
 @NgModule({
   imports: [
@@ -12,6 +13,9 @@ import { ConversorComponent } from './components';
   ],
   exports: [
   	ConversorComponent
+  ],
+  providers: [
+  	MoedaService
   ]
 })
 export class ConversorModule { }
