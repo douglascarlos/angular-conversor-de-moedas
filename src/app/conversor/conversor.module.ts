bc00d41056d518fcd04b352b922addd2b43b25d4
Douglas Carlos <dnevescarlos@gmail.com>
2018-01-17 01:39:02 -0200

    criando e implementando pipe para transformar padrão da data

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -7,6 +7,7 @@ import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
 import { NumeroDirective } from './directives';
 import { ModalCotacaoComponent } from './utils';
+import { DataBrPipe } from './pipes';
 
 @NgModule({
   imports: [
@@ -17,7 +18,8 @@ import { ModalCotacaoComponent } from './utils';
   declarations: [
   	ConversorComponent,
   	NumeroDirective,
-  	ModalCotacaoComponent
+  	ModalCotacaoComponent,
+  	DataBrPipe
   ],
   exports: [
   	ConversorComponent
