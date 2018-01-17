b1bfc95c3122ba661541316c8280f2d3892a2db7
Douglas Carlos <dnevescarlos@gmail.com>
2018-01-17 01:18:40 -0200

    implementando modal

--- src/app/conversor/utils/modal-cotacao.component.ts
+++ src/app/conversor/utils/modal-cotacao.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { Conversao, ConversaoResponse } from '../models';
+import { ConversorService } from '../services';
 
 @Component({
   selector: 'modal-cotacao',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalCotacaoComponent implements OnInit {
 
-  constructor() { }
+	@Input() id: string;
+	@Input() conversaoResponse: ConversaoResponse;
+	@Input() conversao: Conversao = new Conversao();
+	@Output() onConfirm: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor(private conversorService: ConversorService) { }
 
   ngOnInit() {
+  }
+
+  private get valorConvertido(): string {
+  	if(this.conversaoResponse == undefined){
+  		return '0';
+  	}
+  	return (this.conversao.valor * this.conversaoResponse.rates[this.conversao.moedaPara]).toFixed(2);
+  }
+
+  private get cotacaoPara(): number {
+  	return this.conversorService.cotacaoPara(this.conversaoResponse, this.conversao);
+  }
+
+  private get cotacaoDe(): string {
+  	return this.conversorService.cotacaoDe(this.conversaoResponse, this.conversao);
+  }
+
+  private get dataCotacao(): string {
+  	return this.conversorService.dataCotacao(this.conversaoResponse);
+  }
+
+  private novaConsulta(): void {
+  	this.onConfirm.emit();
   }
 
 }
